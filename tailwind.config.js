d6db8f8b78fa21f605fbf8aa0e7a6fadd827e63c
Stefanus Petra <stefanuspetra.p@gmail.com>
2022-04-28 22:01:49 +0700

    update code

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors:{
+        primary: '#143F6B',
+        second: '#FEB139',
+        tertiary: '#F55353',
+      },
+      screen:{
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+ 
